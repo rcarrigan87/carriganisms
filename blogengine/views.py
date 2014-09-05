682a277d3224f5b2dd42db1697a0146d549f91dc
Ryan Carrigan <Rcarrigan87@gmail.com>
2014-09-05 17:39:53 -0400

    added pagination!

--- blogengine/views.py
+++ blogengine/views.py
@@ -1,6 +1,7 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
@@ -8,9 +9,22 @@ def index(request):
 
 	context = RequestContext(request)
 
-	post_list = Post.objects.order_by('-pub_date')[:5]
+	post_list = Post.objects.order_by('-pub_date') #order all blog posts by published date
 
-	context_dict = {'posts':post_list}
+	paginator = Paginator(post_list,2) #show 5 blog posts
+
+	page = request.GET.get('page')
+
+	try:
+		post = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		post = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		post = paginator.page(paginator.num_pages)
+
+	context_dict = {'posts':post}
 
 	return render_to_response('blogengine/blog_homepage.html', context_dict, context)
 
