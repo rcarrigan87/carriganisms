2285e27a0061ed3a8b740d7e8044465110ed65db
Ryan Carrigan <Rcarrigan87@gmail.com>
2014-09-17 15:29:49 -0400

    installed static wtfffff

--- carriganisms/settings/heroku.py
+++ carriganisms/settings/heroku.py
@@ -15,7 +15,7 @@ DATABASES = {'default': dj_database_url.config()}
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Static asset configuration
 STATIC_ROOT = 'staticfiles'
